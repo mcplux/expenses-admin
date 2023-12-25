24d66a211fb16b24eba532d55bc5fea1ae4268fe
Juan Martinez <pablomar01@outlook.com>
2023-12-25 17:45:26 -0500

    Budget.vue created

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
-
+  import Budget from './components/Budget.vue';
 </script>
 
 <template>
   <div>
     <header>
       <h1>Planificador de gastos</h1>
+      <div class="container-header container shadow">
+        <Budget
+
+        />
+      </div>
     </header>
   </div>
 </template>
@@ -49,5 +54,21 @@
     margin: 0;
     color: var(--white);
     text-align: center;
+  }
+  .container {
+    width: 90%;
+    max-width: 80rem;
+    margin: 0 auto;
+  }
+  .container-header {
+    margin-top: -5rem;
+    transform: translateY(5rem);
+    padding: 5rem;
+  }
+  .shadow {
+    box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
+    background-color: var(--white);
+    border-radius: 1.2rem;
+    padding: 5rem;
   }
 </style>
---
+++ src/components/Budget.vue
@@ -0,0 +1,58 @@
+<script setup>
+
+</script>
+
+<template>
+  <form class="budget">
+    <div class="field">
+      <label for="new-budget">Definir presupuesto</label>
+      <input 
+        type="number" 
+        name="new-budget" 
+        id="new-budget"
+        placeholder="Añade tu presupuesto"
+        class="new-budget"
+      />
+    </div>
+    <input type="submit" value="Definir presupuesto">
+  </form>
+</template>
+
+<style scoped>
+  .budget {
+    width: 100%;
+  }
+  .field {
+    display: grid;
+    gap: 2rem;
+  }
+  .field label {
+    font-size: 2.2rem;
+    text-align: center;
+    color: var(--blue);
+  }
+  .budget input[type="number"] {
+    background-color: var(--light-gray);
+    border-radius: 1rem;
+    padding: 1rem;
+    border: none;
+    font-size: 2.2rem;
+    text-align: center;
+  }
+  .budget input[type="submit"] {
+    background-color: var(--blue);
+    border: none;
+    padding: 1rem;
+    text-align: center;
+    font-size: 2rem;
+    margin-top: 2rem;
+    color: var(--white);
+    font-weight: 900;
+    width: 100%;
+    transition: background-color 300ms ease;
+  }
+  .budget input[type="submit"]:hover {
+    background-color: #1048a4;
+    cursor: pointer;
+  }
+</style>
