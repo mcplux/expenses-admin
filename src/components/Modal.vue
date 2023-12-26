ab1d98aac1cd65b9dc90162be115410594f47eed
Juan Martinez <pablomar01@outlook.com>
2023-12-25 20:48:15 -0500

    add new expense form created

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -13,6 +13,46 @@
         @click="$emit('hide-modal')"
       />
     </div>
+    <div class="container">
+      <form class="new-expense">
+        <legend>Añadir gasto</legend>
+        <div class="field">
+          <label for="name">Nombre</label>
+          <input 
+            type="text"
+            id="name"
+            placeholder="Añade el nombre del gasto" 
+          />
+        </div>
+
+        <div class="field">
+          <label for="number">Cantidad</label>
+          <input 
+            type="number"
+            id="number"
+            placeholder="Añade la cantidad del gasto. Ej. 400" 
+          />
+        </div>
+
+        <div class="field">
+          <label for="category">Categoría</label>
+          <select name="category" id="category">
+            <option value="" selected disabled>---Seleccione una opción---</option>
+            <option value="saving">Ahorro</option>
+            <option value="food">Comida</option>
+            <option value="enterteiment">Entretenimiento</option>
+            <option value="health">Salud</option>
+            <option value="subscriptions">Suscripciones</option>
+            <option value="others">Otros</option>
+          </select>
+        </div>
+
+        <input 
+          type="submit" 
+          value="Añadir gasto"
+        />
+      </form>
+    </div>
   </div>
 </template>
 
@@ -32,6 +72,40 @@
   }
   .close-modal img {
     width: 3rem;
+    cursor: pointer;
+
+  }
+  .new-expense {
+    margin: 10rem auto 0 auto;
+    display: grid;
+    gap: 2rem;
+  }
+  .new-expense legend {
+    text-align: center;
+    color: var(--white);
+    font-size: 3rem;
+    font-weight: 700;
+  }
+  .new-expense .field {
+    display: grid;
+    gap: 2rem;
+  }
+  .new-expense input,
+  .new-expense select {
+    background-color: var(--light-gray);
+    border-radius: 1rem;
+    padding: 1rem;
+    border: none;
+    font-size: 2.2rem;
+  }
+  .new-expense label {
+    color: var(--white);
+    font-size: 3rem;
+  }
+  .new-expense input[type="submit"] {
+    background-color: var(--blue);
+    color: var(--white);
+    font-weight: 700;
     cursor: pointer;
   }
 </style>
