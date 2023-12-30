0b31e3b3385e39791a62099e150ed1372750901e
Juan Martinez <pablomar01@outlook.com>
2023-12-29 20:35:42 -0500

    expenses graphic

--- src/components/BudgetControl.vue
+++ src/components/BudgetControl.vue
@@ -1,5 +1,7 @@
 <script setup>
-  import image from '../assets/img/grafico.jpg'
+  import { computed } from 'vue';
+  import CircleProgress from 'vue3-circle-progress'
+  import "vue3-circle-progress/dist/circle-progress.css"
   import { formatMoney } from '../helpers'
 
   defineEmits(['reset-app'])
@@ -18,12 +20,24 @@
       required: true,
     },
   })
+
+  const percent = computed(() => {
+    return parseInt(props.spent / props.budget * 100) 
+  })
 </script>
 
 <template>
   <div class="two-columns">
     <div class="graphic-container">
-      <img :src="image" alt="Gráfico">
+      <p class="percent">{{ percent }}%</p>
+      <CircleProgress 
+        :percent="percent"
+        :size="250"
+        :border-width="25"
+        :border-bg-width="25"
+        fill-color="#3b82f6"
+        empty-color="#e1e1e1"
+      />
     </div>
     <div class="budget-container">
       <button type="button" class="reset-app" @click="$emit('reset-app')">
@@ -49,6 +63,22 @@
 </template>
 
 <style scoped>
+  .graphic-container {
+    position: relative;
+
+  }
+  .percent {
+    position: absolute;
+    margin: auto;
+    top: calc(50% - 1.5rem);
+    left: 0;
+    right: 0;
+    text-align: center;
+    z-index: 100;
+    font-size: 3rem;
+    font-weight: 900;
+    color: var(--dark-gray);
+  }
   .two-columns {
     display: flex;
     flex-direction: column;
